73e1f2c881e1e6736c2ab3658a0dc27cf8f1cd70
Chinh Nguyen <cqnguyen21st@yahoo.com>
2018-11-26 20:34:22 -0800

    fix lints

--- src/app.ts
+++ src/app.ts
@@ -17,10 +17,9 @@ class App {
     // support application/json type post data
     this.app.use(bodyParser.json());
 
-    //support application/x-www-form-urlencoded post data
+    // support application/x-www-form-urlencoded post data
     this.app.use(bodyParser.urlencoded({ extended: false }));
   }
 }
 
 export default new App().app;
-
