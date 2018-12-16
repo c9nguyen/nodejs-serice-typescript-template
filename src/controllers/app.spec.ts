dd2d2c1c0cce8e798db364019e4780d383944305
Chinh Nguyen <cqnguyen21st@yahoo.com>
2018-12-15 22:06:18 -0800

    update name format and controller folder

--- src/controllers/app.spec.ts
+++ src/controllers/app.spec.ts
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
 
-import { AppController } from './app-controller';
+import { AppController } from './app';
 
 /* tslint:disable:no-unused-expression */
 describe('AppController', () => {
